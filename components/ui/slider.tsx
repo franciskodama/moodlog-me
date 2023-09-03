2cbbe70a36c1b7559fbbf07f6ea7c956ea4c3480
Francis Kodama <fk@fkodama.com>
2023-09-03 14:06:06 -0400

    Fix json and styles

--- components/ui/slider.tsx
+++ components/ui/slider.tsx
@@ -17,10 +17,13 @@ const Slider = React.forwardRef<
     )}
     {...props}
   >
-    <SliderPrimitive.Track className='relative h-1 w-full grow overflow-hidden rounded-full bg-primary'>
+    <SliderPrimitive.Track className='relative h-2 w-full grow overflow-hidden rounded-full bg-yellow border-2 border-primary'>
       <SliderPrimitive.Range className='absolute h-full bg-primary' />
     </SliderPrimitive.Track>
-    <SliderPrimitive.Thumb className='block h-4 w-4 bg-yellow rounded-full border-2 border-primary ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50' />
+    <SliderPrimitive.Thumb
+      className='block h-4 w-4 bg-blue rounded-full border-2 border-primary shadow-sm shadow-primary
+    ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50'
+    />
   </SliderPrimitive.Root>
 ));
 Slider.displayName = SliderPrimitive.Root.displayName;
